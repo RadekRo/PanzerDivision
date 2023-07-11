af936501ff4cd892a0dbb05cd1882aedcedbdbfc
RadekRo <r.roclawski@ankara.pl>
2023-07-11 21:26:57 +0200

    Created starting app project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PanzerDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
