c776f88fb7ac16042defbe83ce4fd95cc3c4e781
RadekRo <r.roclawski@ankara.pl>
2023-07-19 21:36:18 +0200

    Added method to add fuel to vehicles

--- Model/Vehicle.cs
+++ Model/Vehicle.cs
@@ -4,4 +4,10 @@ public abstract class Vehicle
 {
     public int Id { get; set; }
     public List<Soldier> Soldiers { get; set; } = new List<Soldier>();
+    private int FuelLevel;
+
+    public void AddFuel(int qty)
+    {
+        FuelLevel += qty;
+    }
 }
